c1499e5448f7d5bc71047e1cfb582a925a84b8e0
BastianRC <abenty.18@hotmail.com>
2022-10-04 20:23:11 +0200

    Updates

--- PHP/proyecto12/app/controllers/LoginController.php
+++ PHP/proyecto12/app/controllers/LoginController.php
@@ -49,7 +49,39 @@ class LoginController extends Controller
                 if ( ! $this->model->existsEmail($email)) {
                     array_push($errors, 'El correo electrónico no existe en la base de datos');
                 } else {
-                    $this->model->sendEmail($email);
+                    if ($this->model->sendEmail($email)) {
+
+                        $data = [
+                            'titulo' => 'Cambio de contraseña de acceso',
+                            'menu' => false,
+                            'errors' => [],
+                            'subtitle' => 'Cambio de contraseña de acceso',
+                            'text' => 'Se ha enviado un correo a <b>' . $email . '</b> para que pueda cambiar su clave de acceso. <br>No olvide revisar su carpeta de spam. <br>Cualquier duda que tenga puede comunicarse con nosotros.',
+                            'color' => 'alert-success',
+                            'url' => 'login',
+                            'colorButton' => 'btn-success',
+                            'textButton' => 'Regresar',
+                        ];
+
+                        $this->view('mensaje', $data);
+
+                    } else {
+
+                        $data = [
+                            'titulo' => 'Error con correo',
+                            'menu' => false,
+                            'errors' => [],
+                            'subtitle' => 'Error en el envío del correo electrónico',
+                            'text' => 'Existió un problema al enviar el correo electrónico.<br>Por favor, pruebe más tarde o comuníquese con nuestro servicio de soporte',
+                            'color' => 'alert-danger',
+                            'url' => 'login',
+                            'colorButton' => 'btn-danger',
+                            'textButton' => 'Regresar',
+                        ];
+
+                        $this->view('mensaje', $data);
+
+                    }
                 }
             }
 
@@ -65,7 +97,6 @@ class LoginController extends Controller
             }
 
         }
-
     }
 
     public function registro()
@@ -192,5 +223,125 @@ class LoginController extends Controller
 
             $this->view('register', $data);
         }
+    }
+
+    public function changePassword($id)
+    {
+        $errors = [];
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            $id = $_POST['id'] ?? '';
+            $password1 = $_POST['password1'] ?? '';
+            $password2 = $_POST['password2'] ?? '';
+
+            if ($id == '') {
+                array_push($errors, 'El usuario no existe');
+            }
+            if ($password1 == '') {
+                array_push($errors, 'La contraseña es requerida');
+            }
+            if ($password2 == '') {
+                array_push($errors, 'Repetir contraseña es requerido');
+            }
+            if ($password1 != $password2) {
+                array_push($errors, 'Ambas claves deben ser iguales');
+            }
+
+            if (count($errors)) {
+
+                $data = [
+                    'titulo' => 'Cambiar contraseña',
+                    'menu'   => false,
+                    'errors' => $errors,
+                    'data' => $id,
+                    'subtitle' => 'Cambia tu contraseña de acceso',
+                ];
+
+                $this->view('changepassword', $data);
+
+            } else {
+
+                if ($this->model->changePassword($id, $password1)) {
+
+                    $data = [
+                        'titulo' => 'Cambiar contraseña',
+                        'menu'   => false,
+                        'errors' => [],
+                        'subtitle' => 'Modificación de la contraseña de acceso',
+                        'text' => 'La contraseña ha sido cambiada correctamente. Bienvenido de nuevo',
+                        'color' => 'alert-success',
+                        'url' => 'login',
+                        'colorButton' => 'btn-success',
+                        'textButton' => 'Regresar',
+                    ];
+
+                    $this->view('mensaje', $data);
+
+                } else {
+
+                    $data = [
+                        'titulo' => 'Error al cambiar contraseña',
+                        'menu'   => false,
+                        'errors' => [],
+                        'subtitle' => 'Error al modificar la contraseña de acceso',
+                        'text' => 'Existió un error al modificar la clave de acceso',
+                        'color' => 'alert-danger',
+                        'url' => 'login',
+                        'colorButton' => 'btn-danger',
+                        'textButton' => 'Regresar',
+                    ];
+
+                    $this->view('mensaje', $data);
+                }
+            }
+        } else {
+            $data = [
+                'titulo' => 'Cambiar contraseña',
+                'menu'   => false,
+                'data' => $id,
+                'subtitle' => 'Cambia tu contraseña de acceso',
+            ];
+
+            $this->view('changepassword', $data);
+        }
+    }
+
+    public function verifyUser()
+    {
+        $errors = [];
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            $user = $_POST['user'] ?? '';
+            $password = $_POST['password'] ?? '';
+            $remember = isset($_POST['remember']) ? 'on' : 'off';
+
+            $errors = $this->model->verifyUser($user, $password);
+
+            $dataForm = [
+                'user' => $user,
+                'remember' => $remember,
+            ];
+
+            if ( ! $errors ) {
+                print 'Bienvenid@';
+            } else {
+                $data = [
+                    'titulo' => 'Login',
+                    'menu'   => false,
+                    'errors' => $errors,
+                    'data' => $dataForm,
+                ];
+                $this->view('login', $data);
+            }
+
+        } else {
+
+            $this->index();
+
+        }
+
+
     }
 }
--- PHP/proyecto12/app/inicio.php
+++ PHP/proyecto12/app/inicio.php
@@ -6,6 +6,7 @@ ini_set('display_errors', 1);
 
 define('ROOT', DIRECTORY_SEPARATOR);
 define('APP', ROOT . 'app' . DIRECTORY_SEPARATOR);
+define('ENCRIPTKEY', 'elperrodesanroque');
 
 // Carga las clases iniciales
 require_once('libs/Mysqldb.php');
