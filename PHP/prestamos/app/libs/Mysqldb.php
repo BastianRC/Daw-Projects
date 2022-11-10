d887a5ab190bd5861ef5cf614c441dfb0268f7a2
Bastian <abenty.18@hotmail.com>
2022-11-10 15:24:37 +0100

    Update

---
+++ PHP/prestamos/app/libs/Mysqldb.php
@@ -0,0 +1,48 @@
+<?php
+
+class Mysqldb
+{
+    private $host = 'mysql';
+    private $user = 'default';
+    private $pass = 'secret';
+    private $dbname = 'tiendamvc';
+
+    private static $instancia = null;
+    private $db = null;
+
+    private function __construct()
+    {
+        $options = [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+        ];
+
+        try {
+
+            $this->db = new PDO(
+                'mysql:host=' . $this->host . ';dbname=' . $this->dbname,
+                $this->user,
+                $this->pass,
+                $options
+            );
+
+        } catch (PDOException $error) {
+
+            exit('La base de datos no está accesible');
+        }
+    }
+
+    public static function getInstance()
+    {
+        if (is_null(self::$instancia)) {
+            self::$instancia = new Mysqldb();
+        }
+
+        return self::$instancia;
+    }
+
+    public function getDatabase()
+    {
+        return $this->db;
+    }
+}
